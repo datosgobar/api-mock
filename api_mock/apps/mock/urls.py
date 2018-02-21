bfe4c9779c83b0c725be89f8755b0b84171b644c
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-02-21 10:12:24 -0300

    Implemento un endpoint echo

--- api_mock/apps/mock/urls.py
+++ api_mock/apps/mock/urls.py
@@ -4,5 +4,6 @@ import api_mock.apps.mock.views as views
 
 # pylint: disable=invalid-name
 urlpatterns = [
+    path('echo/', views.echo, name='echo'),
     path('', views.landing),
 ]
--- api_mock/apps/mock/views.py
+++ api_mock/apps/mock/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 # Create your views here.
 def landing(request):
     return render(request, 'landing.html')
+
+
+@api_view(['POST'])
+def echo(request):
+    return Response(request.data)
