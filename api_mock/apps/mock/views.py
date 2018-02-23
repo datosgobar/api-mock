83d09fed551c9c34250220ce86395abb0d614dc2
Sebastian Gonzalez <sgonzalez@devartis.com>
2018-02-23 15:00:28 -0300

    Agrego manejo de requests en /docs/oai_specification
        refs:#7
    
        refs:#7

--- api_mock/apps/mock/views.py
+++ api_mock/apps/mock/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
+from rest_framework_yaml.renderers import YAMLRenderer
+
+from api_mock.apps.mock import oai
 
 
 # Create your views here.
@@ -32,3 +35,14 @@ def datos_json(request):
                                    'username': 'john'}]}
 
     return Response(response_data)
+
+
+@api_view(['GET'])
+@renderer_classes((YAMLRenderer,))
+def oai_specification_yml(_):
+    return Response(oai.specification)
+
+
+@api_view(['GET'])
+def oai_specification_json(_):
+    return Response(oai.specification)
